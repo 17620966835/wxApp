f54b50e16121c6a514a8f3fef4700367138c6a76
LJR <chelsey_xx@163.com>
2020-03-04 07:23:43 +0800

    注册小程序和页面

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -1,20 +1,31 @@
 //app.js
+// 注册一个小程序示例
 App({
+  //小程序 初始化完成时
   onLaunch: function () {
-    
-    if (!wx.cloud) {
-      console.error('请使用 2.2.3 或以上的基础库以使用云能力')
-    } else {
-      wx.cloud.init({
-        // env 参数说明：
-        //   env 参数决定接下来小程序发起的云开发调用（wx.cloud.xxx）会默认请求到哪个云环境的资源
-        //   此处请填入环境 ID, 环境 ID 可打开云控制台查看
-        //   如不填则使用默认环境（第一个创建的环境）
-        // env: 'my-env-id',
-        traceUser: true,
-      })
-    }
+    // 获取用户信息
+  },
+  // 显示出来时
+  onShow:function(options){
+  //1. 判断小程序进入场景
 
-    this.globalData = {}
+  // 2.获取用户信息后，将用户的信息传递给服务器
+  wx.getUserInfo({
+    success:function(res){
+      console.log(res);
+    }
+  })
+  },
+  // 隐藏时
+  onHide:function(){
+
+  },
+  // 产生错误时
+  onError:function(msg){
+
+  },
+  globalData:{
+    name:'jack平',
+    age:18
   }
 })
--- miniprogram/pages/home/home.js
+++ miniprogram/pages/home/home.js
@@ -1,6 +1,19 @@
 // pages/home/home.js
-Page({
+// getApp()获取app()产生的示例对象
+const app=getApp()
+// console.log(app.globalData.name);
+// console.log(app.globalData.age);
 
+// const name=app.globalData.name;
+// const age=app.globalData.age;
+
+
+// 注册一个页面
+// 页面也有自己的生命周期函数
+Page({
+  handleGetUserInfo(event){
+    console.log(event);
+  },
   /**
    * 页面的初始数据
    */
@@ -11,7 +24,8 @@ Page({
       {id:110,name:"远远",age:18},
       { id: 110, name: "平平",age:18 },
     ],
-    counter:0
+    counter:0,
+    list:[]
   },
   handleBtnClick(){
   //错误
@@ -25,11 +39,22 @@ Page({
     this.setData({
       counter:this.data.counter - 1
     })
-  },
+  }, 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    console.log('onLoad')
+    wx.request({
+      url:'http://123.207.32.32:8000/api/wh/recommend',
+      success:(res)=>{
+        console.log(res);
+        const data=res.data.data.list;
+        this.setData({
+          list:data
+        })
+      }
+    })
 
   },
 
@@ -41,7 +66,7 @@ Page({
   },
 
   /**
-   * 生命周期函数--监听页面显示
+   * 生命周期函数--监听页面显示出来时
    */
   onShow: function () {
 

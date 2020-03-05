5be8dd5288f339ce1d12596f032ebd763e3dfca9
LJR <chelsey_xx@163.com>
2020-03-05 15:30:14 +0800

    事件处理

--- miniprogram/pages/home/home.js
+++ miniprogram/pages/home/home.js
@@ -5,9 +5,56 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    titles:['衣服','鞋子','包包']
   },
-
+  btnClick(){
+    console.log('按钮发生点击')
+  },
+  touchStart(){
+    console.log('touchStart')
+  },
+  touchMove(){
+    console.log('touchMove')
+  },
+  touchEnd(){
+    console.log('touchEnd')
+  },
+  Tap(){
+    console.log('Tap')
+  },
+  longPress(){
+    console.log('longPress')
+  },
+  handleEventClick(event){
+    console.log('---',event)
+  },
+  handleItemClick(event){
+    console.log(event)
+    //title - index
+    const dataset=event.currentTarget.dataset;
+    const title=dataset.item;
+    const index=dataset.index;
+    console.log(title,index);
+  },
+  // ---------------事件冒泡和事件捕获--------------------
+  handleCaptureView1(){
+    console.log('handleCaptureView1')
+  },
+  handleBindView1(){
+    console.log('handleBindView1')
+  },
+  handleCaptureView2() { 
+    console.log('handleCaptureView2')
+  },
+  handleBindView2() {
+    console.log('handleBindView2')
+   },
+  handleCaptureView3() {
+    console.log('handleCaptureView3')
+   },
+  handleBindView3() {
+    console.log('handleBindView3')
+   },
   /**
    * 生命周期函数--监听页面加载
    */

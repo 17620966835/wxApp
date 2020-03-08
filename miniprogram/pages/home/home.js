6a68d82d94ec5ed0e6d82b626c12336a6becdcaa
LJR <chelsey_xx@163.com>
2020-03-08 08:43:02 +0800

    弹窗和分享

--- miniprogram/pages/home/home.js
+++ miniprogram/pages/home/home.js
@@ -7,7 +7,63 @@ Page({
   data: {
 
   },
+  handleShowToast(){
+    // console.log('---')
+    wx.showToast({
+      title:'毛毛平',
+      duration:2000,
+      icon:'loading',
+      // image:"/assets/icon/icon.png",
+      mask:true,
+      success:function(){
+        console.log('毛毛平展示成功')
+      },
+      fail:function(){
+        consloe.log('毛毛平展示成败')
+      },
+      complete:function(){
+        console.log('完成毛毛平函数的调用')
+      }
+    })
+  },
+  handleShowModal(){
+    wx.showModal({
+      title:'明明白白毛毛平',
+      content:'确定穿着品如的衣服吗？',
+      // showCancel:true,
+      cancelText:"退寻找柜",
+      cancelColor:"red",
+      success:function(res){
+        console.log(res)
+        if(res.cancel){
+          console.log('毛毛平已脱下')
+        }
+        if(res.confirm){
+          console.log('毛毛平已穿上')
+        }
+      }
+    })
+  },
+  handleShowLoading(){
+    wx.showLoading({
+      title:'找衣柜中....',
+      mask:true
+    })
 
+    setTimeout(()=>{
+      // 必须手动
+      wx.hideLoading()
+    },1000)
+  },
+  handleShowAction(){
+    wx.showActionSheet({
+      itemList: ['艾莉','品如'],
+      itemColor:'red',
+      success:function(res){
+        console.log(res)
+      },
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
@@ -60,7 +116,11 @@ Page({
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
-
+  onShareAppMessage: function (options) {
+    return {
+      title:'你好啊',
+      path:'/pages/about/about',
+ imageUrl:'https://goss4.cfp.cn/creative/vcg/800/version23/VCG41670895271.jpg?x-oss-process=image/format,jpg/interlace,1'
+    }
   }
 })
